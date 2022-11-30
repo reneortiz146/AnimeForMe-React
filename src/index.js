6f09e466d7366bcfce606352e20f152848c96483
Rene Ortiz <reneortizzrey@gmail.com>
2022-11-29 18:42:08 -0600

    Renderizacion en Index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
-const contenidoIndex = document.getElementById("root");
+const contenidoIndex = document.getElementById("footer");
 const containerIndex = ReactDOM.createRoot(contenidoIndex);
-containerIndex.render();
+containerIndex.render(
+  <div>
+    <div class="col1">
+      <p>
+        Terms and Conditions &middot; Privacy
+        <br />
+        &copy; AnimeForMe 2022. Todos los derechos reservados.
+      </p>
+    </div>
+    <div class="col2">
+      <p>
+        Address: <strong>Chihuahua, Chihuahua, Mexico</strong>
+        <br />
+        Phone: <strong>+52 (123) 456 789</strong>
+      </p>
+    </div>
+    <div class="col3">
+      <p>
+        Diseñado por: Equipo x <br />
+        Instituo Tecnologico de Chihuahua II
+      </p>
+    </div>
+  </div>
+  //<App />
+);
